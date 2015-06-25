cb364276bcaca9766b002ee4b406275b70862dbe
Alec Arme & Toby Alden <student@epicodus.com>
2015-06-25 15:52:54 -0700

    replace php code

--- web/index.php
+++ web/index.php
@@ -1,20 +1,12 @@
-<?php
+<?php //The name of this file in this example is imgdata.php
 
-require('../vendor/autoload.php');
-
-$app = new Silex\Application();
-$app['debug'] = true;
-
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
-
-// Our web handlers
-
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return 'Hello';
-});
-
-$app->run();
+  // $url=$_GET['url'];
+  // $img = file_get_contents($url);
+  // $fn = substr(strrchr($url, "/"), 1);
+  // file_put_contents($fn,$img);
+  // echo $fn;
+  //
+  // $head = array_change_key_case(get_headers('https://i.ytimg.com/vi/xS3wQEGlOGU/1.jpg', TRUE));
+  // $filesize = $head['content-length'];
+  echo "hello world!";
+?>
